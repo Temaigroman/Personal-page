621ec803bf1c2cd5d582ecd21b76dc0691341f7e
Temaigroman <152429841+Temaigroman@users.noreply.github.com>
2024-06-02 18:38:37 +0300

    без роутов

--- calculation/src/Page/Region/Region.jsx
+++ calculation/src/Page/Region/Region.jsx
@@ -1,33 +1,23 @@
 import Main_component from "../../Сomponents/Main/Main_component";
 import { regions } from "../../Lists/Region_list/regionList";
-import { NavLink, useParams } from "react-router-dom";
-
 
 const Region = () => {
-  const {id} = useParams();
-	
-  return (
+
+  return ( 
     <div className="chose">
-      {regions.map((region, index) => {
-      
-        if (region.id === parseInt(id, 10)) {
-          return (
-            <NavLink to={`/region/${region.id}`}>
-              <Main_component
-                key={index}
-                title={region.title}
-                index={index}
-              />
-            </NavLink>
-          );
-        }
-        return null; // Если регион не соответствует, возвращаем пустой элемент
-      })}
+  
+        {regions.map((location, index)=>{
+        return <Main_component key={index} title={location.title}/>
+         
+      })}  
+              
     </div>
   );
-};
+  }
+   
 
-export default Region;
+
+  export default Region;
 
 
 
